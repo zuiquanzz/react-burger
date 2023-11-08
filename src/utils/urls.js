b5b93dd69ac1bfaef039697e853662d828f4e494
zuiquanzz <m.pashkovsky@crystals.ru>
2023-11-09 00:51:37 +0300

    mini fix with check resp

--- src/utils/urls.js
+++ src/utils/urls.js
@@ -17,5 +17,5 @@ export const passwordResetReset = "/reset"
 
 
 export function  checkResponse(res) {
-  return res.ok ? res.json() : Promise.reject(`Ошибка ${res.status}`)
+  return res.ok ? res.json() : res.json().then((err) => Promise.reject(err));
 }
