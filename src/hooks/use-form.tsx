9626107b4a86e216dad935ca1856b6edb342c451
zuiquanzz <m.pashkovsky@crystals.ru>
2023-11-19 05:24:41 +0300

    hooks ok

--- src/hooks/use-form.tsx
+++ src/hooks/use-form.tsx
@@ -1,10 +1,10 @@
-import {useState} from "react";
+import {ChangeEvent, useState} from "react";
 
 export const useForm = (inputValues={}) => {
   const [values, setValues] = useState(inputValues);
   const [formChanged, setFormChanged] = useState(false)
 
-  const handleChange = (event) => {
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
     const {value, name} = event.target;
     setValues({...values, [name]: value});
     setFormChanged(true)
